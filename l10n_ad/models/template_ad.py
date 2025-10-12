677ad5d2742a5097c9ad18fae708a350c4f94fa4
Dídac Alsina | IPGrup <93338796+dalsina@users.noreply.github.com>
2025-10-12 19:09:29 +0200

    [UPD] Actualizar las referencias de cuentas en el método _get_ad_template_data

--- l10n_ad/models/template_ad.py
+++ l10n_ad/models/template_ad.py
@@ -8,8 +8,8 @@ class AccountChartTemplate(models.AbstractModel):
     @template('ad')
     def _get_ad_template_data(self):
         return {
-            'property_account_receivable_id': 'account_common_430',
-            'property_account_payable_id': 'account_common_400',
+            'property_account_receivable_id': 'l10n_ad.account_common_430',
+            'property_account_payable_id': 'l10n_ad.account_common_400',
             'code_digits': '8',
             'currency_id': 'base.EUR',
             'country_id': 'base.ad',
