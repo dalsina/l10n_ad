7e64d96ab13a5e2bbcd0ff176c767bcce989eec5
dalsina <dalsina@ipgrup.com>
2025-10-24 11:52:24 +0200

    [ADD] create __manifest__.py files for Andorra - Comptabilitat module in hr_payroll and reports

---
+++ l10n_ad_hr_payroll/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Andorra - Comptabilitat",
+    "author": "IPGrup x IdeAnd",
+    "category": "Accounting/Localizations/Account Charts",
+    "version": "19.0.1.0.0",
+    'icon': '',
+    'countries': ['ad'],
+    'depends': [
+    ],
+    "data": [
+    ],
+    'installable': True,
+    'l10n': True,
+    'license': 'LGPL-3',
+}
