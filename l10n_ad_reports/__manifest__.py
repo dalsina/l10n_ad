33cb6e2cade752a5993153485981ddee0e8e95f9
dalsina <dalsina@ipgrup.com>
2025-11-03 23:03:59 +0100

    [ADD] update module name and add balance sheet report data for Andorra

--- l10n_ad_reports/__manifest__.py
+++ l10n_ad_reports/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Andorra - Comptabilitat",
+    "name": "Andorra - Informes Comptabilitat",
     "author": "IPGrup x IdeAnd",
     "category": "Accounting/Localizations/Account Charts",
     "version": "19.0.1.0.0",
@@ -8,8 +8,9 @@
     'depends': [
     ],
     "data": [
+        'data/balance_sheet_report_data.xml',
     ],
+    'auto_install': ['l10n_ad', 'account_reports'],
     'installable': True,
-    'l10n': True,
     'license': 'LGPL-3',
 }
